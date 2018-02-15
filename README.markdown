65fb892392c7ae4c822763088c70801bc91c3b97
Shawn Davis <shawn@develmaycare.com>
2018-02-14 19:47:30 -0500

    Initial Import

---
+++ README.markdown
@@ -0,0 +1,21 @@
+# Twitter Bootstrap 4 for Django HTMGel
+
+Support for Twitter Bootstrap 4 in [Django HTMGel][django-htmgel].
+
+[django-htmgel]: https://github.com/develmaycare/django-htmgel/
+
+## Install
+
+To install:
+
+    pip install django-htmgel[bootstrap4]; # TODO
+    pip install git+https://github.com/develmaycare/django-htmgel-bootstrap4.git
+
+Add the app to your ``INSTALLED_APPS``:
+
+	INSTALLED_APPS = [
+		...
+		'htmgel',
+		'htmgel_bootstrap4',
+		...
+	]
---
+++ setup.py
@@ -0,0 +1,43 @@
+# See https://packaging.python.org/en/latest/distributing.html
+# and https://docs.python.org/2/distutils/setupscript.html
+# and https://pypi.python.org/pypi?%3Aaction=list_classifiers
+from setuptools import setup, find_packages
+
+
+def read(path):
+    with open(path, "rb") as f:
+        contents = f.read()
+        f.close()
+    return contents
+
+setup(
+    name='django-htmgel-bootstrap4',
+    version=read("VERSION.txt"),
+    description=read("DESCRIPTION.txt"),
+    long_description=read("README.markdown"),
+    author='Shawn Davis',
+    author_email='shawn@develmaycare.com',
+    url='https://github.com/develmaycare/django-htmgel-bootstrap4',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "BeautifulSoup",
+        "django-htmgel",
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Framework :: Django',
+    ],
+    zip_safe=False,
+    tests_require=["BeautifulSoup"],
+    test_suite='runtests.runtests'
+)
